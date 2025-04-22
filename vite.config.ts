a7306c1920164f38d62eff4fe3933597e8bcb90c
mansqw123 <userkumaruser02@gmail.com>
2025-04-22 11:06:17 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/snow-day-chance-finder/",  // Added for GitHub Pages correct base path
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
-
+export default defineConfig({
+  base: '/snow-day-chance-finder/',  // यही लाइन वेबसाइट सही करने के लिए जरूरी है
+  plugins: [react()],
+})
